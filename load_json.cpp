85c169b9d4ff33d13bd466c3fd6bcb51b9e6e016
Heming Huang <heming@umich.edu>
2021-04-02 10:25:00 -0400

    load_json func

--- load_json.cpp
+++ load_json.cpp
@@ -2,12 +2,19 @@
 #include <jsoncpp/json/json.h>
 #include <fstream>
 
-int main() {
-    std::ifstream ifs("first_20_frame_optical_res.json");
+// Function to read json file
+Json::Value read_json(std::string json_file) {
+    std::ifstream ifs(json_file);
     Json::Reader reader;
     Json::Value obj;
     reader.parse(ifs, obj);
-    std::cout << obj[0]["bb"]<<std::endl;
-    return 1;
+    return obj;
+}
+
+// Test
+int main() {
+    Json::Value obj = read_json("first_20_frame_optical_res.json");
+    std::cout<<obj[0]["bb"][0]<<std::endl;
+    return 0;
 }
 
