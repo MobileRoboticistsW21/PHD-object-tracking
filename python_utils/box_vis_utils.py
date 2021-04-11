a6a3caf0fcbca8e2258cb865c12d6edd65017813
Bahaa Aldeeb <baldeeb@umich.edu>
2021-04-10 21:28:39 -0400

    Included Miracle's visualization notebook

---
+++ python_utils/box_vis_utils.py
@@ -0,0 +1,14 @@
+def xywh_to_corners(data):
+    """
+    Takes a coordinate xywh as input
+    Return tlbr as output
+    Note that input must be of size (1,4) to use
+    """
+    data = np.array(data)
+    data = data.astype(float)
+    data[:,0] = data[:,0] - data[:,2]/2
+    data[:,1] = data[:,1] - data[:,3]/2
+    data[:,2] = data[:,0] + data[:,2]
+    data[:,3] = data[:,1] + data[:,3]
+
+    return data
